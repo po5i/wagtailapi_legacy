ca4777f96dc03b58775ba291f78401fe83a5bd83
Carlos Villavicencio <carlos.po5i@gmail.com>
2018-05-10 09:18:43 -0500

    Fixed typo and bump version

--- wagtailapi_legacy/v1/apps.py
+++ wagtailapi_legacy/v1/apps.py
@@ -6,15 +6,15 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 class WagtailAPIAppConfig(AppConfig):
-    name = 'wagtailapi_legacy_v1'
-    label = 'wagtailapi_legacy_v1'
+    name = 'wagtailapi_legacy.v1'
+    label = 'wagtailapi_legacy.v1'
     verbose_name = "Wagtail API"
 
     def ready(self):
         # Install cache purging signal handlers
         if getattr(settings, 'WAGTAILAPI_USE_FRONTENDCACHE', False):
             if apps.is_installed('wagtail.contrib.wagtailfrontendcache'):
-                from wagtailapi_legacy_v1.signal_handlers import register_signal_handlers
+                from wagtailapi_legacy.v1.signal_handlers import register_signal_handlers
                 register_signal_handlers()
             else:
                 raise ImproperlyConfigured(
